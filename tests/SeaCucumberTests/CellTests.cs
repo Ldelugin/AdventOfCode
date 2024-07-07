faf338540a92e9cf47a2e30fe0fcfc599fa07dc8
laurent <laurentd@q-free.com>
2024-07-08 00:02:58 +0200

    Added cell tests

---
+++ tests/SeaCucumberTests/CellTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeaCucumber;
+
+namespace SeaCucumberTests;
+
+[TestClass]
+public class CellTests
+{
+    [TestMethod]
+    [DataRow(Cell.Empty)]
+    [DataRow(Cell.EastFacing)]
+    [DataRow(Cell.SouthFacing)]
+    public void Constructor_SetsValue_AsGiven(char value)
+    {
+        // Arrange
+        var cell = new Cell(value);
+        
+        // Act
+        var actualValue = cell.Value;
+        
+        // Assert
+        Assert.AreEqual(value, actualValue);
+    }
+}
