4869ed39e7c8c806e2a0a173634ab9cb7557012c
laurent <laurentd@q-free.com>
2024-07-08 00:04:15 +0200

    Added simulator to report steps until no movement is detected

---
+++ src/SeaCucumber/SeaCucumberSimulator.cs
@@ -0,0 +1,17 @@
+﻿namespace SeaCucumber;
+
+public class SeaCucumberSimulator
+{
+    private readonly Grid map;
+
+    public SeaCucumberSimulator(Grid map)
+    {
+        this.map = map ?? throw new ArgumentNullException(nameof(map));
+    }
+    
+    public int SimulateUntilNoMovementsDetected()
+    {
+        var steps = 0;
+        return steps;
+    }
+}
