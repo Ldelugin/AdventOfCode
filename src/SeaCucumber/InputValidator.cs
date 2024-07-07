37096c842c42a6ec5e8e4f81f91473dad9d1bda8
laurent <laurentd@q-free.com>
2024-07-08 00:00:54 +0200

    Added InputValidator and tests

---
+++ src/SeaCucumber/InputValidator.cs
@@ -0,0 +1,61 @@
+﻿namespace SeaCucumber;
+
+public class InputValidator
+{
+    private readonly string[]? input;
+
+    public InputValidator(string[]? input)
+    {
+        this.input = input;
+    }
+
+    public bool Validate(out string? notValidReason)
+    {
+        // input should not be null
+        if (this.input == null)
+        {
+            notValidReason = "The input is null";
+            return false;
+        }
+
+        // input should not be empty
+        if (input.Length == 0)
+        {
+            notValidReason = "The input is empty";
+            return false;
+        }
+
+        var rows = this.input.Length;
+        var columns = this.input[0].Length;
+
+        for (var i = 0; i < rows; i++)
+        {
+            var line = this.input[i];
+            // input each row should have the same amount of columns
+            if (line.Length != columns)
+            {
+                notValidReason = $"Line '{i}' was expected to have a length of {columns}, actual length {line.Length}";
+                return false;
+            }
+            
+            for (var j = 0; j < columns; j++)
+            {
+                var character = line[j];
+                // each character should be either '.', '>' or 'v'
+                switch (character)
+                {
+                    case Cell.Empty:
+                    case Cell.EastFacing:
+                    case Cell.SouthFacing:
+                        break;
+                    default:
+                        notValidReason = $"Character {character} was expected to be {Cell.Empty}, {Cell.EastFacing} or {Cell.SouthFacing}";
+                        return false;
+                }
+            }
+        }
+
+        notValidReason = null;
+        return true;
+    }
+}
---
+++ tests/SeaCucumberTests/InputValidatorTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeaCucumber;
+
+namespace SeaCucumberTests;
+
+[TestClass]
+public class InputValidatorTests
+{
+    [TestMethod]
+    public void Validate_ReturnsFalse_WhenInputIsNull()
+    {
+        // Arrange
+        string[]? input = null;
+        var inputValidator = new InputValidator(input);
+        
+        // Act
+        var isValid = inputValidator.Validate(out var notValidReason);
+        
+        // Assert
+        Assert.IsFalse(isValid);
+        Assert.IsNotNull(notValidReason);
+        Assert.IsTrue(notValidReason.Contains("input") && notValidReason.Contains("null"));
+    }
+    
+    [TestMethod]
+    public void Validate_ReturnsFalse_WhenInputIsEmpty()
+    {
+        // Arrange
+        var input = Array.Empty<string>();
+        var inputValidator = new InputValidator(input);
+        
+        // Act
+        var isValid = inputValidator.Validate(out var notValidReason);
+        
+        // Assert
+        Assert.IsFalse(isValid);
+        Assert.IsNotNull(notValidReason);
+        Assert.IsTrue(notValidReason.Contains("input") && notValidReason.Contains("empty"));
+    }
+    
+    [TestMethod]
+    public void Validate_ReturnsFalse_WhenInputAsInconsistentLinesLength()
+    {
+        // Arrange
+        var input = new[]
+        {
+            "..>>..",
+            "..>.."
+        };
+        var inputValidator = new InputValidator(input);
+        
+        // Act
+        var isValid = inputValidator.Validate(out var notValidReason);
+        
+        // Assert
+        Assert.IsFalse(isValid);
+        Assert.IsNotNull(notValidReason);
+        Assert.IsTrue(notValidReason.Contains("Line") && notValidReason.Contains("expected to have a length"));
+    }
+    
+    [TestMethod]
+    public void Validate_ReturnsFalse_WhenInputContainCharactersNotExpected()
+    {
+        // Arrange
+        var input = new[]
+        {
+            "..>>..",
+            "..>x.."
+        };
+        var inputValidator = new InputValidator(input);
+        
+        // Act
+        var isValid = inputValidator.Validate(out var notValidReason);
+        
+        // Assert
+        Assert.IsFalse(isValid);
+        Assert.IsNotNull(notValidReason);
+        Assert.IsTrue(notValidReason.Contains("Character") && notValidReason.Contains("expected to be"));
+    }
+
+    [TestMethod]
+    public void Validate_ReturnsTrue_WhenInputIsValid()
+    {
+        // Arrange
+        var input = new[]
+        {
+            "..>>..",
+            "..vv..",
+            "v.>v.."
+        };
+        var inputValidator = new InputValidator(input);
+        
+        // Act
+        var isValid = inputValidator.Validate(out var notValidReason);
+        
+        // Assert
+        Assert.IsTrue(isValid);
+        Assert.IsNull(notValidReason);
+    }
+}
