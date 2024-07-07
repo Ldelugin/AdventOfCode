fc13064096c0520b8360dd0de07096b70f0622d6
laurent <laurentd@q-free.com>
2024-07-08 00:04:34 +0200

    Add test to verify the expected steps

---
+++ tests/SeaCucumberTests/SeaCucumberSimulatorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeaCucumber;
+
+namespace SeaCucumberTests;
+
+[TestClass]
+public class SeaCucumberSimulatorTests
+{
+    [TestMethod]
+    public void SimulateUntilNoMovementsDetected_Returns58_WithExampleInput()
+    {
+        // Arrange
+        const string inputFileName = "example-input.txt";
+        const int expectedSteps = 58;
+
+        var inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, inputFileName);
+        var input = File.ReadAllLines(inputFilePath);
+        var map = new Grid(input);
+        var simulator = new SeaCucumberSimulator(map);
+        
+        // Act
+        var steps = simulator.SimulateUntilNoMovementsDetected();
+        
+        // Assert
+        Assert.AreEqual(expectedSteps, steps);
+    }
+}
