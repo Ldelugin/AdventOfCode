8d9ae6cc98c2c7e3cd039175f63db5351948b196
laurent <laurentd@q-free.com>
2024-07-08 01:35:36 +0200

    Corrected grid

--- src/SeaCucumber/Grid.cs
+++ src/SeaCucumber/Grid.cs
@@ -13,7 +13,67 @@ public class Grid
         this.ParseInput(input);
     }
     
-    public Cell[,] Value { get; }
+    public Cell[,] Value { get; private set; }
+
+    public bool MoveEastFacing()
+    {
+        var moved = false;
+        var newGrid = this.CreateGridClone();
+
+        for (var i = 0; i < this.rows; i++)
+        {
+            for (var j = 0; j < this.columns; j++)
+            {
+                if (this.Value[i, j].Value != Cell.EastFacing)
+                {
+                    continue;
+                }
+
+                var nextJ = (j + 1) % this.columns;
+                if (this.Value[i, nextJ].Value != Cell.Empty)
+                {
+                    continue;
+                }
+
+                newGrid[i, j].SetEmpty();
+                newGrid[i, nextJ].SetEastFacing();
+                moved = true;
+            }
+        }
+
+        this.Value = newGrid;
+        return moved;
+    }
+
+    public bool MoveSouthFacing()
+    {
+        var moved = false;
+        var newGrid = this.CreateGridClone();
+
+        for (var i = 0; i < this.rows; i++)
+        {
+            for (var j = 0; j < this.columns; j++)
+            {
+                if (this.Value[i, j].Value != Cell.SouthFacing)
+                {
+                    continue;
+                }
+
+                var nextI = (i + 1) % this.rows;
+                if (this.Value[nextI, j].Value != Cell.Empty)
+                {
+                    continue;
+                }
+
+                newGrid[i,j].SetEmpty();
+                newGrid[nextI, j].SetSouthFacing();
+                moved = true;
+            }
+        }
+        
+        this.Value = newGrid;
+        return moved;
+    }
 
     private void ParseInput(string[] input)
     {
@@ -26,57 +86,16 @@ public class Grid
         }
     }
 
-    public bool MoveEastFacing()
+    private Cell[,] CreateGridClone()
     {
-        var moved = false;
+        var newGrid = new Cell[this.rows, this.columns];
         for (var i = 0; i < this.rows; i++)
         {
             for (var j = 0; j < this.columns; j++)
             {
-                var cell = this.Value[i, j];
-                var destinationCell = this.Value[i, (j + 1) % this.columns];
-                if (cell.Value != Cell.EastFacing || destinationCell.Value != Cell.Empty)
-                {
-                    continue;
-                }
-
-                cell.SetEmpty();
-                destinationCell.SetEastFacing();
-                moved = true;
-                // skip the next row since it just moved, but only if i is the last row to avoid skipping the first row of the next iteration
-                if (j < this.columns - 1)
-                {
-                    j++;
-                }
+                newGrid[i, j] = new Cell(this.Value[i, j].Value);
             }
         }
-        return moved;
-    }
-
-    public bool MoveSouthFacing()
-    {
-        var moved = false;
-        for (var i = 0; i < this.rows; i++)
-        {
-            for (var j = 0; j < this.columns; j++)
-            {
-                var cell = this.Value[i, j];
-                var destinationCell = this.Value[(i + 1) % this.rows, j];
-                if (cell.Value != Cell.SouthFacing || destinationCell.Value != Cell.Empty)
-                {
-                    continue;
-                }
-                
-                cell.SetEmpty();
-                destinationCell.SetSouthFacing();
-                moved = true;
-                // skip the next row since it just moved, but only if i is the last row to avoid skipping the first row of the next iteration
-                if (i < rows - 1)
-                {
-                    i++;
-                }
-            }
-        }
-        return moved;
+        return newGrid;
     }
 }
