548734ea6f647c4814d93bd654435d0d1656c1b2
laurent <laurentd@q-free.com>
2024-07-08 00:03:37 +0200

    Added program of SeaCucumber and input file

---
+++ src/SeaCucumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using SeaCucumber;
+
+const string inputFileName = "input.txt";
+
+var inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, inputFileName);
+var input = File.ReadAllLines(inputFilePath);
+var inputValidator = new InputValidator(input);
+var isValid = inputValidator.Validate(out var notValidReason);
+
+if (!isValid)
+{
+    throw new InvalidOperationException($"The input is not valid: {notValidReason}");
+}
+
+var grid = new Grid(input);
