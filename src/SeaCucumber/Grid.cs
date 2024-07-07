4b6da0b818505d2e9d8c60b6ada5df206c92bb58
laurent <laurentd@q-free.com>
2024-07-08 00:03:06 +0200

    Added grid

---
+++ src/SeaCucumber/Grid.cs
@@ -0,0 +1,27 @@
+﻿namespace SeaCucumber;
+
+public class Grid
+{
+    private readonly int rows;
+    private readonly int columns;
+    private readonly Cell[,] grid;
+
+    public Grid(string[] input)
+    {
+        this.rows = input.Length;
+        this.columns = input[0].Length;
+        this.grid = new Cell[this.rows, this.columns];
+        this.ParseInput(input);
+    }
+
+    private void ParseInput(string[] input)
+    {
+        for (var i = 0; i < this.rows; i++)
+        {
+            for (var j = 0; j < this.columns; j++)
+            {
+                this.grid[i, j] = new Cell(input[i][j]);
+            }
+        }
+    }
+}
