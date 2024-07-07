d6231a8a60eaac4cf60d77cba97a6aa91682fb6c
laurent <laurentd@q-free.com>
2024-07-08 00:02:51 +0200

    Added cell

---
+++ src/SeaCucumber/Cell.cs
@@ -0,0 +1,15 @@
+﻿namespace SeaCucumber;
+
+public class Cell
+{
+    public const char Empty = '.';
+    public const char EastFacing = '>';
+    public const char SouthFacing = 'v';
+    
+    public Cell(char value)
+    {
+        this.Value = value;
+    }
+    
+    public char Value { get; }
+}
