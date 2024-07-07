f715ab77141981eb0c0ff45d7a3ecc37b2ec9319
laurent <laurentd@q-free.com>
2024-07-08 01:05:35 +0200

    Added grid tests

---
+++ tests/SeaCucumberTests/Extensions/AssertExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeaCucumber;
+
+namespace SeaCucumberTests.Extensions;
+
+public static class AssertExtensions
+{
+    public static void GridsAreEqual(this Assert _, Cell[,] left, Cell[,] right)
+    {
+        var rows = left.GetLength(0);
+        var columns = left.GetLength(1);
+        
+        Assert.AreEqual(rows, right.GetLength(0));
+        Assert.AreEqual(columns, right.GetLength(1));
+
+        for (var i = 0; i < rows; i++)
+        {
+            for (var j = 0; j < columns; j++)
+            {
+                Assert.AreEqual(left[i, j].Value, right[i, j].Value);
+            }
+        }
+    }
+}
---
+++ tests/SeaCucumberTests/GridTests.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeaCucumber;
+using SeaCucumberTests.Extensions;
+
+namespace SeaCucumberTests;
+
+[TestClass]
+public class GridTests
+{
+    [TestMethod]
+    public void Constructor_ParsesInput_Correctly()
+    {
+        // Arrange
+        var input = new[]
+        {
+            "...>>>>>...",
+            "...>>>.....",
+            "...>>......",
+        };
+        var expectedGrid = new Cell[,]
+        {
+            { new('.'), new('.'), new('.'), new('>'), new('>'), new('>'), new('>'), new('>'), new('.'), new('.'), new('.') },
+            { new('.'), new('.'), new('.'), new('>'), new('>'), new('>'), new('.'), new('.'), new('.'), new('.'), new('.') },
+            { new('.'), new('.'), new('.'), new('>'), new('>'), new('.'), new('.'), new('.'), new('.'), new('.'), new('.') }
+        };
+        
+        // Act
+        var grid = new Grid(input);
+        
+        // Assert
+        Assert.That.GridsAreEqual(expectedGrid, grid.Value);
+    }
+
+    [TestMethod]
+    public void MoveEastFacing_ReturnsTrue_WhenAtLeastOneEastFacingMoved()
+    {
+        // Arrange
+        var input = new[]
+        {
+            ".>..",
+            "....",
+            ">...",
+            "...>"
+        };
+        var expectedGrid = new Cell[,]
+        {
+            { new('.'), new('.'), new('>'), new('.') },
+            { new('.'), new('.'), new('.'), new('.') },
+            { new('.'), new('>'), new('.'), new('.') },
+            { new('>'), new('.'), new('.'), new('.') }
+        };
+        var grid = new Grid(input);
+        
+        // Act
+        var hasMoved = grid.MoveEastFacing();
+        
+        // Assert
+        Assert.IsTrue(hasMoved);
+        Assert.That.GridsAreEqual(expectedGrid, grid.Value);
+    }
+
+    [TestMethod]
+    public void MoveEastFacing_ReturnsFalse_WhenNoEastFacingMoved()
+    {
+        // Arrange
+        var input = new[]
+        {
+            ".>>v",
+            ">v.>",
+            ".v.."
+        };
+        var expectedGrid = new Cell[,]
+        {
+            { new('.'), new('>'), new('>'), new('v') },
+            { new('>'), new('v'), new('.'), new('>') },
+            { new('.'), new('v'), new('.'), new('.') }
+        };
+        var grid = new Grid(input);
+        
+        // Act
+        var hasMoved = grid.MoveEastFacing();
+        
+        // Assert
+        Assert.IsFalse(hasMoved);
+        Assert.That.GridsAreEqual(expectedGrid, grid.Value);
+    }
+
+    [TestMethod]
+    public void MoveSouthFacing_ReturnsTrue_WhenAtLeastOneSouthFacingMoved()
+    {
+        // Arrange
+        var input = new[]
+        {
+            "...",
+            "v..",
+            ".v."
+        };
+        var expectedGrid = new Cell[,]
+        {
+            { new('.'), new('v'), new('.') },
+            { new('.'), new('.'), new('.') },
+            { new('v'), new('.'), new('.') }
+        };
+        
+        var grid = new Grid(input);
+        
+        // Act
+        var hasMoved = grid.MoveSouthFacing();
+        
+        // Assert
+        Assert.IsTrue(hasMoved);
+        Assert.That.GridsAreEqual(expectedGrid, grid.Value);
+    }
+
+    [TestMethod]
+    public void MoveSouthFacing_ReturnsFalse_WhenNoSouthFacingMoved()
+    {
+        // Arrange
+        var input = new[]
+        {
+            ".>>v",
+            ">v.>",
+            ".v.."
+        };
+        var expectedGrid = new Cell[,]
+        {
+            { new('.'), new('>'), new('>'), new('v') },
+            { new('>'), new('v'), new('.'), new('>') },
+            { new('.'), new('v'), new('.'), new('.') }
+        };
+        var grid = new Grid(input);
+        
+        // Act
+        var hasMoved = grid.MoveSouthFacing();
+        
+        // Assert
+        Assert.IsFalse(hasMoved);
+        Assert.That.GridsAreEqual(expectedGrid, grid.Value);
+    }
+}
