9404cdd5698e3bf0b038035508ca6fa114a31cd2
laurentd <laurentd@q-free.com>
2024-07-08 13:57:12 +0200

    Added code commentary

--- src/SonarSweep/Program.cs
+++ src/SonarSweep/Program.cs
@@ -1,17 +1,15 @@
 ﻿using SonarSweep;
 using SonarSweep.Measurements;
 
+// The file name, this could also be a command line argument
 const string reportFileName = "input.txt";
+
+// The full path to the report file
 var reportFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, reportFileName);
 
+// Set up the processor and run it
 var measurementReader = new MeasurementReader();
 var measurementCalculator = new MeasurementCalculator();
 var console = new ConsoleWrapper();
 var processor = new MeasurementProcessor(measurementReader, measurementCalculator, console);
 processor.Process(reportFilePath);
-
-// Keep the original code for comparison
-var reportInspector = new ReportInspector(reportFileName);
-var totalIncreases = reportInspector.InspectTotalIncreases();
-
-Console.WriteLine($"The total increases of the '{reportFileName}' report = {totalIncreases}");
