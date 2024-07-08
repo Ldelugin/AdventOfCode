af23eb56b75ab9df512267784bf685389dfc14e8
laurentd <laurentd@q-free.com>
2024-07-08 13:49:09 +0200

    Implemented test to assert the Process method

--- tests/SonarSweepTests/Measurements/MeasurementProcessorTests.cs
+++ tests/SonarSweepTests/Measurements/MeasurementProcessorTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SonarSweep;
@@ -39,5 +40,29 @@ public class MeasurementProcessorTests
     {
         Assert.ThrowsException<ArgumentNullException>(() => new MeasurementProcessor(this.measurementReaderMock.Object,
             this.measurementCalculatorMock.Object, console: null));
+    }
+
+    private static IEnumerable<object[]> ProcessData()
+    {
+        yield return new object[] { new List<int> { 1, 2, 3 }, "input1.txt", 2 };
+        yield return new object[] { new List<int> { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 }, "input2.txt", 7 };
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(ProcessData), DynamicDataSourceType.Method)]
+    public void Process_ShouldCalculateIncreasesCorrectly(List<int> measurementsToReturn, string filePath, int expectedIncreases)
+    {
+        // Arrange
+        this.measurementReaderMock.Setup(mock => mock.ReadMeasurements(filePath)).Returns(measurementsToReturn);
+        this.measurementCalculatorMock.Setup(mock => mock.CalculateIncreases(measurementsToReturn)).Returns(expectedIncreases);
+        
+        // Act
+        this.processor.Process();
+
+        // Assert
+        this.measurementReaderMock.Verify(mock => mock.ReadMeasurements(filePath), Times.Once);
+        this.measurementCalculatorMock.Verify(mock => mock.CalculateIncreases(measurementsToReturn), Times.Once);
+        this.consoleMock.Verify(mock => mock.WriteLine(It.Is<string>(
+            message => message.Contains(expectedIncreases.ToString()))), Times.Once);
     }
 }
