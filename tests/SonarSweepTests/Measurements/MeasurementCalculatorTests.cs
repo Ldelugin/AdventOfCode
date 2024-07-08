8a89ee3f65d8179c4c470a14e94291346943dc51
laurentd <laurentd@q-free.com>
2024-07-08 13:02:09 +0200

    Implemented dynamic data as method instead property

--- tests/SonarSweepTests/Measurements/MeasurementCalculatorTests.cs
+++ tests/SonarSweepTests/Measurements/MeasurementCalculatorTests.cs
@@ -9,16 +9,16 @@ public class MeasurementCalculatorTests
 {
     private readonly MeasurementCalculator calculator = new();
     
-    private static object[] calculateIncreasesData =
+    private static IEnumerable<object[]> CalculateIncreasesData()
     {
-        new object[] { new List<int>(), 0 },
-        new object[] { new List<int> { 1, 2, 3, 4, 5, 6, 7 }, 7 },
-        new object[] { new List<int> { 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1 }, 7 },
-        new object[] { new List<int> { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 }, 7 }
-    };
+        yield return new object[] { new List<int>(), 0 };
+        yield return new object[] { new List<int> { 1, 2, 3, 4, 5, 6, 7 }, 7 };
+        yield return new object[] { new List<int> { 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1 }, 7 };
+        yield return new object[] { new List<int> { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 }, 7 };
+    }
 
     [TestMethod]
-    [DynamicData(nameof(calculateIncreasesData))]
+    [DynamicData(nameof(CalculateIncreasesData), DynamicDataSourceType.Method)]
     public void CalculateIncreases_ShouldReturnCorrectCount(List<int> measurements, int expected)
     {
         // Act
