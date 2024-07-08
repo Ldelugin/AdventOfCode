0b0c111d11e44d2bdac30f58cab3d944a9144a17
laurentd <laurentd@q-free.com>
2024-07-08 13:55:50 +0200

    Implemented the MeasurementProcessor

--- src/SonarSweep/Program.cs
+++ src/SonarSweep/Program.cs
@@ -1,7 +1,16 @@
 ﻿using SonarSweep;
+using SonarSweep.Measurements;
 
 const string reportFileName = "input.txt";
+var reportFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, reportFileName);
 
+var measurementReader = new MeasurementReader();
+var measurementCalculator = new MeasurementCalculator();
+var console = new ConsoleWrapper();
+var processor = new MeasurementProcessor(measurementReader, measurementCalculator, console);
+processor.Process(reportFilePath);
+
+// Keep the original code for comparison
 var reportInspector = new ReportInspector(reportFileName);
 var totalIncreases = reportInspector.InspectTotalIncreases();
 
