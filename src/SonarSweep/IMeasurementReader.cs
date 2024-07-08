e128251feb9d530766bc15cc2c80c20351b01b9c
laurentd <laurentd@q-free.com>
2024-07-08 12:34:48 +0200

    Added IMeasurementReader
    I am going to work on improving the existing code to be more inline with best practices.

---
+++ src/SonarSweep/IMeasurementReader.cs
@@ -0,0 +1,16 @@
+namespace SonarSweep;
+
+/// <summary>
+/// An interface for reading measurements from a file.
+/// </summary>
+public interface IMeasurementReader
+{
+    /// <summary>
+    /// Reads measurements from a file.
+    /// </summary>
+    /// <param name="filePath">
+    /// The path to the file containing the measurements.
+    /// </param>
+    /// <returns>A list of measurements.</returns>
+    List<int> ReadMeasurements(string filePath);
+}
