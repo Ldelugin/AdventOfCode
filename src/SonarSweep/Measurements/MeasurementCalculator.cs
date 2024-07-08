14dad7b141f66209641f1330181e0cf9df027554
laurentd <laurentd@q-free.com>
2024-07-08 12:38:56 +0200

    Added implementations, still throwing NotImplementedException

---
+++ src/SonarSweep/Measurements/MeasurementCalculator.cs
@@ -0,0 +1,21 @@
+namespace SonarSweep.Measurements;
+
+/// <summary>
+/// A class that calculates the number of increases in a list of measurements.
+/// </summary>
+public class MeasurementCalculator : IMeasurementCalculator
+{
+    /// <summary>
+    /// Calculates the number of increases in a list of measurements.
+    /// </summary>
+    /// <param name="measurements">
+    /// The list of measurements to calculate the increases for.
+    /// </param>
+    /// <returns>
+    /// The number of increases in the list of measurements.
+    /// </returns>
+    public int CalculateIncreases(List<int> measurements)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/SonarSweep/Measurements/MeasurementReader.cs
@@ -0,0 +1,19 @@
+namespace SonarSweep.Measurements;
+
+/// <summary>
+/// A class that reads measurements from a file.
+/// </summary>
+public class MeasurementReader : IMeasurementReader
+{
+    /// <summary>
+    /// Reads measurements from a file.
+    /// </summary>
+    /// <param name="filePath">
+    /// The path to the file containing the measurements.
+    /// </param>
+    /// <returns>A list of measurements.</returns>
+    public List<int> ReadMeasurements(string filePath)
+    {
+        throw new NotImplementedException();
+    }
+}
